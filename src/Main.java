93d358287dc17167eace1713b29b8ee90e03ba84
Ali <alisallllllar@gmail.com>
2025-06-17 10:16:46 +0330

    ffuf

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame  = new JFrame("Hello World!!!");
+        frame.setVisible(true);
+//        JPanel panel = new JPanel(" ");
+    }
+}
